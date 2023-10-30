3fc68df4192c01c05880fda4f0b4b2783742be0e
Arvid Lindström <arvid-lindstrom@live.se>
2023-10-30 08:37:39 +0100

    All data moved out of functions

--- scripts.js
+++ scripts.js
@@ -10,17 +10,18 @@ const partyColors = {
   ovr:  '#808080',
 }
 
-const stackedData = {
+
+const currentData = {
   results: [ '', 
-      7.6, 
-      5.2,  
-      38.6,  
-      4.0,  
-      3.0,  
-      18.4,  
-      2.6, 
-      19.0,  
-      1.5, ],
+      8.1, 
+      4.2,  
+      37.9,  
+      4.1,  
+      2.3,  
+      16.1,  
+      2.9, 
+      22.0,  
+      1.8, ],
   seriesNames:  ['Parti', 
       'V',  
       'MP',
@@ -31,20 +32,42 @@ const stackedData = {
       'KD', 
       'SD', 
       'Övr', ],
+  changeSinceLast: [
+      0.5,
+      -0.4, 
+      -0.7,
+      0.1,
+      -0.7,
+      -2.3,
+      0.3,
+      3,0,
+      0.3,
+      0.3,
+      -0.5, ],
+  month: "September",
+  year: 2023,
+  history: [
+    ['Tidpunkt','Vänsterpartiet','Miljöpartiet','Socialdemokraterna','Centerpartiet','Liberalerna','Moderaterna','Kristdemokraterna','Sverigedemokraterna','Övriga'],
+    ['Valresultatet 2022',6.8,5.1,30.3,6.7,4.6,19.1,5.3,20.5,1.5],
+    ['Juni (2023)', 7.8,5.0,36.6,4.3,3.0,18.5,4.1,18.3,2.4],
+    ['September (2023)',7.6,5.2,38.6,4.0,3.0,18.4,2.6,19.0,1.5],
+    ['Oktober (2023)',8.1,4.8,37.9,4.1,2.3,16.1,2.9,22.0,1.8],
+  ],
+  historyByBlock: [
+    ['Tidpunkt','M/L/KD/SD','S/V/MP/C'],
+    ['Valresultatet 2022', 49.5, 48.9],
+    ['Juni (2023)', 43.9, 53.7],
+    ['September (2023)', 43.0, 55.4],
+    ['Oktober (2023)', 43.3, 54.9],
+  ],
 }
 
+
+// Constructor for options object
 function Options() {
-  this.title = 'Väljarbarometer september 2023',
+  this.title = `Väljarbarometer ${currentData.month} ${currentData.year}`,
   this.colors = Object.values(partyColors);
 }
-
-
-const optionsStacked = {
-  'title':'Väljarbarometer september 2023',
-  'isStacked': 'percent',
-  colors: Object.values(partyColors),
-};
-
 
 
 
@@ -53,12 +76,15 @@ google.charts.load('current', {'packages':['corechart', 'table'],'language': 'sv
 
 // Set a callback to run when the Google Visualization API is loaded.
 // Can instead be specified in load above
-google.charts.setOnLoadCallback(drawStacked);
-google.charts.setOnLoadCallback(drawBarsV);
-google.charts.setOnLoadCallback(drawLines);
-google.charts.setOnLoadCallback(drawLinesByBlock);
+google.charts.setOnLoadCallback(drawCharts);
 
 
+function drawCharts() {
+  drawStacked();
+  drawBarsV();
+  drawLines();
+  drawLinesByBlock();
+}
 
 
 // CALLBACKS TO DRAW CHARTS
@@ -66,20 +92,18 @@ google.charts.setOnLoadCallback(drawLinesByBlock);
 
 function drawStacked() {
 
-  // Create the data table, becomes private to function 
-  // so other drawfunctions can reuse "data" but w other data
-  const data = new google.visualization.arrayToDataTable([
-    stackedData.seriesNames,
-    stackedData.results,
-  ]);
-
   const options = new Options();
   options.isStacked = 'percent';
+
+  
+  const data = new google.visualization.arrayToDataTable([
+    currentData.seriesNames,
+    currentData.results,
+  ]);
 
   // Instantiate and draw charts, passing in different data but same options.
   const chart = new google.visualization.BarChart(document.getElementById('stackedChart'));
   chart.draw(data, options);
-
 }
 
 
@@ -91,16 +115,16 @@ function drawBarsV() {
   options.legend = {position: 'none'};
 
   const data = google.visualization.arrayToDataTable([
-    ['Parti', 'Andel %', {role: 'style'}],
-    ['V', 7.6, options.colors[0]],
-    ['MP', 5.2, options.colors[1]],
-    ['S', 38.6, options.colors[2]],
-    ['C', 4.0, options.colors[3]],
-    ['L', 3.0, options.colors[4]],
-    ['M', 18.4, options.colors[5]],
-    ['KD', 2.6, options.colors[6]],
-    ['SD', 19.0, options.colors[7]],
-    ['Övr', 1.5, options.colors[8]]
+    ['Parti', 'Andel %', {role: 'style'}, {role: 'tooltip'}],
+    ['V', currentData.results[1], options.colors[0], `Förändring sedan senaste mätning ${currentData.changeSinceLast[0]}`],
+    ['MP', currentData.results[2], options.colors[1], `Förändring sedan senaste mätning ${currentData.changeSinceLast[1]}`],
+    ['S', currentData.results[3], options.colors[2], `Förändring sedan senaste mätning ${currentData.changeSinceLast[2]}`],
+    ['C', currentData.results[4], options.colors[3], `Förändring sedan senaste mätning ${currentData.changeSinceLast[3]}`],
+    ['L', currentData.results[5], options.colors[4], `Förändring sedan senaste mätning ${currentData.changeSinceLast[4]}`],
+    ['M', currentData.results[6], options.colors[5],`Förändring sedan senaste mätning ${currentData.changeSinceLast[5]}`],
+    ['KD', currentData.results[7], options.colors[6], `Förändring sedan senaste mätning ${currentData.changeSinceLast[6]}`],
+    ['SD', currentData.results[8], options.colors[7], `Förändring sedan senaste mätning ${currentData.changeSinceLast[7]}`],
+    ['Övr', currentData.results[9], options.colors[8], `Förändring sedan senaste mätning ${currentData.changeSinceLast[8]}`]
   ]);
 
   
@@ -110,7 +134,7 @@ function drawBarsV() {
                    sourceColumn: 1,
                    type: "string",
                    role: "annotation" },
-                 2]);
+                 2, 3]);
 
 
   const chart = new google.visualization.ColumnChart(document.getElementById('barChartV'));
@@ -124,20 +148,10 @@ function drawLines() {
 
   const options = new Options();
  
-  const data = new google.visualization.arrayToDataTable([
-    ['Tidpunkt','Vänsterpartiet','Miljöpartiet','Socialdemokraterna','Centerpartiet','Liberalerna','Moderaterna','Kristdemokraterna','Sverigedemokraterna','Övriga'],
-    ['Valresultatet 2022',6.8,5.1,30.3,6.7,4.6,19.1,5.3,20.5,1.5],
-    ['Juni (2023)', 7.8,5.0,36.6,4.3,3.0,18.5,4.1,18.3,2.4],
-    ['September (2023)',7.6,5.2,38.6,4.0,3.0,18.4,2.6,19.0,1.5],
-    ['Oktober (2023)',8.1,4.8,37.9,4.1,2.3,16.1,2.9,22.0,1.8],
-    //['Förändring sedan föregående mätning',0.5,-0.4,-0.7,0.1,-0.7,-2.3,0.3,3,0,0.3,0.3,-0.5],
-  ]);
-
-
+  const data = new google.visualization.arrayToDataTable(currentData.history);
 
   const chart = new google.visualization.LineChart(document.getElementById('lineChart'));
   chart.draw(data, options);
-
 }
 
 
@@ -148,16 +162,8 @@ function drawLinesByBlock() {
   const options = new Options();
   options.colors = [partyColors.m, partyColors.s];
 
-  const data = new google.visualization.arrayToDataTable([
-    ['Tidpunkt','M/L/KD/SD','S/V/MP/C'],
-    ['Valresultatet 2022', 49.5, 48.9],
-    ['Juni (2023)', 43.9, 53.7],
-    ['September (2023)', 43.0, 55.4],
-    ['Oktober (2023)', 43.3, 54.9],
-  ]);
-
+  const data = new google.visualization.arrayToDataTable(currentData.historyByBlock);
 
   const chart = new google.visualization.LineChart(document.getElementById('lineChartByBlock'));
   chart.draw(data, options);
-
 }
