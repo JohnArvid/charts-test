4a8b17e34cc86c3c555a0672cd0b153159452e72
Arvid Lindström <arvid-lindstrom@live.se>
2023-11-02 07:43:58 +0100

    Housekeeping
    + Added file for basic css
    + Linebreaks in js
    + Added class to container div in html

--- scripts.js
+++ scripts.js
@@ -45,7 +45,16 @@ const partyData = {
   month: "Oktober",
   year: 2023,
   history: [
-    ['Tidpunkt','Vänsterpartiet','Miljöpartiet','Socialdemokraterna','Centerpartiet','Liberalerna','Moderaterna','Kristdemokraterna','Sverigedemokraterna','Övriga'],
+    ['Tidpunkt',
+      'Vänsterpartiet',
+      'Miljöpartiet',
+      'Socialdemokraterna', 
+      'Centerpartiet', 
+      'Liberalerna', 
+      'Moderaterna', 
+      'Kristdemokraterna', 
+      'Sverigedemokraterna', 
+      'Övriga'],
     ['Valresultatet 2022',6.8,5.1,30.3,6.7,4.6,19.1,5.3,20.5,1.5],
     ['Juni (2023)', 7.8,5.0,36.6,4.3,3.0,18.5,4.1,18.3,2.4],
     ['September (2023)',7.6,5.2,38.6,4.0,3.0,18.4,2.6,19.0,1.5],
@@ -72,7 +81,12 @@ const blockData = {
 
 const ministerData = {
   results: [45,  18,  17,  20],
-  seriesNames: ['Magdalena Andersson', 'Ulf Kristersson', 'Jimmie Åkesson',  'Inget av alternativen'],
+  seriesNames: [
+    'Magdalena Andersson', 
+    'Ulf Kristersson', 
+    'Jimmie Åkesson',  
+    'Inget av alternativen',
+    ],
   seriesColors: {
     s: partyData.seriesColors.s,
     m: partyData.seriesColors.m,
@@ -80,7 +94,12 @@ const ministerData = {
     ovr: partyData.seriesColors.ovr,
   },
   history: [
-    ['Tidpunkt','Magdalena Andersson', 'Ulf Kristersson', 'Jimmie Åkesson',  'Inget av alternativen'],
+    ['Tidpunkt',
+      'Magdalena Andersson', 
+      'Ulf Kristersson', 
+      'Jimmie Åkesson',  
+      'Inget av alternativen',
+      ],
     ['September (2023)',  48,  22,  13,  17],
     ['Oktober (2023)',  45,  18,  17,  20],
     ],
@@ -158,12 +177,23 @@ function drawBarsV(dataObject, chartId) {
 
 
   const dataArray = dataObject.seriesNames.map( (party, i) => {
-    return [party, dataObject.results[i], dataObject.results[i], Object.values(dataObject.seriesColors)[i], `Förändring sedan senaste mätning ${dataObject.changeSinceLast[i]}` ];
+    return (
+      [party, dataObject.results[i], 
+        dataObject.results[i], 
+        Object.values(dataObject.seriesColors)[i], 
+        `Förändring sedan senaste mätning ${dataObject.changeSinceLast[i]}` ]
+      );
 })
   
 
   const data = google.visualization.arrayToDataTable([
-    ['Parti', 'Andel %', {role: 'annotation'}, {role: 'style'}, {role: 'tooltip'}],...dataArray ]);
+    ['Parti', 'Andel %', 
+      {role: 'annotation'}, 
+      {role: 'style'}, 
+      {role: 'tooltip'}
+      ],
+    ...dataArray 
+    ]);
 
 
   const chart = new google.visualization.ColumnChart(document.getElementById(chartId));
